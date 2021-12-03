48eef6277623d55f2f58cb174e243793416e2e6e
Robert Ngo <41568472+robertncl@users.noreply.github.com>
2021-12-03 16:08:12 +0800

    Update fstab.py

--- fstab.py
+++ fstab.py
@@ -7,19 +7,29 @@ with open("input.yaml", "r") as input:
     try:
         #load the yaml input into a dict
         fstab_input = yaml.safe_load(input)
-        #remove the first level of this input
+        
+        #remove the first level of the input
         fstab_list = list(fstab_input.values())
 
         output = open("fstab.txt","a")
-        #iterate of the nested dict inside the list
+        
+        #iterate through the nested dict inside the list
         for dic in fstab_list:
             for key, val in dic.items():
+                
+                #first write the device
                 output.write(key + " ")
+                
+                #iterate through the dict and join the value into a string to append into the output
                 for i in val.values():
                     listToStr = ''.join(map(str, i))
                     output.write(listToStr)
                     output.write(" ")
+                    
+                #add newline at the end of the loop
                 output.write("\n")
+                
         output.close()
+        
     except yaml.YAMLError as exc:
         print(exc)
